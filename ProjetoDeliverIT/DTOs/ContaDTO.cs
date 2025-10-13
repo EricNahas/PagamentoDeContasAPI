a7a042587db548b7bd842c0a85226edae38c3860
Eric Nahas <nahaseric@gmail.com>
2025-10-13 20:53:03 -0300

    ContaDTO e AutoMapping

---
+++ ProjetoDeliverIT/DTOs/ContaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoDeliverIT.DTOs
+{
+    public class ContaDTO
+    {
+        public string? Nome { get; set; }                
+        public decimal ValorOriginal { get; set; }      
+        public decimal ValorCorrigido { get; set; }     
+        public int DiasAtraso { get; set; }             
+        public DateTimeOffset DataPagamento { get; set; }     
+    }
+}
---
+++ ProjetoDeliverIT/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProjetoDeliverIT.DTOs;
+using ProjetoDeliverIT.Models;
+
+namespace ProjetoDeliverIT.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Conta, ContaDTO>().ReverseMap();
+        }
+    }
+}
